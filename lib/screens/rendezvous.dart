281581c8f860b11dbad83be8314eec0d71a75dc3
MOKHTARRR <mohamedmokhtar.romdhani@supcom.tn>
2022-01-12 22:58:30 +0100

    ....

--- lib/screens/rendezvous.dart
+++ lib/screens/rendezvous.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/profile.dart';
 
 class RendezVous extends StatefulWidget {
   @override
@@ -8,6 +9,33 @@ class RendezVous extends StatefulWidget {
 class _RendezVousState extends State<RendezVous> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 70,
+        backgroundColor: Colors.red,
+        title: const Text(
+          "book reseration",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+            letterSpacing: 1,
+          ),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => Profile()),
+          ),
+        ),
+      ),
+      body: Column(
+        children: const [
+          SizedBox(height: 20),
+        ]
+      ),
+    );
   }
 }
